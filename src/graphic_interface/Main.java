7893899771083489de005d4ef215e0946859074e
ishvatov <ishvatov@gmail.com>
2018-10-26 21:51:57 +0300

    Commit by S.Hvatov

---
+++ src/graphic_interface/Main.java
@@ -0,0 +1,28 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("menu.fxml"));
+        primaryStage.setTitle("FaceHunter");
+        primaryStage.setScene(new Scene(root, 788, 589));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+
+
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
